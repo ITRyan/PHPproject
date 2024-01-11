995d0e7bd26a513d22dfbaf77abe1b317cc11a6d
ITRyan <itryanchan@gmail.com>
2024-01-11 08:29:30 -0800

    BUG FIX

--- admin/code.php
+++ admin/code.php
@@ -4,17 +4,39 @@ include("../config/function.php");
 
 function InsertCode($tableName, $data)
 {
-
+$count = 0;
     global $conn;
     $table = validate($tableName);
     $colums = array_keys($data);
     $values = array_values($data);
 
     $finalColumn = implode(',', $colums);
-    $finalValues = "'" . implode("',", $values) . "'";
+    // print_r("'".implode("'", $values).",'");exit;
+
+    // $finalValues = "'". implode("',", $values) . "',";
+    // print_r($finalValues);exit;
+    $finalValues = '';
+foreach ($values as $key => $value) {
+
+    if($count != count($values) - 1){
+        $finalValues .= "'";
+
+$finalValues.=$value;
+$finalValues .= "'";
+$finalValues .= ",";
+
+    }
+    else{
+
+        $finalValues.='"'.$value.'"';
+
+    }
+    $count++;
+}
+
       //check the ooutput
-    $query = "INSERT INTO $table ($finalColumn)VALUES($finalValues) ";
-    print_r($query);exit;
+    $query = "INSERT INTO $table ($finalColumn)  VALUES ($finalValues) ";
+    // print_r($query);exit;
     $result = mysqli_query($conn, $query);
     return $result;
 }
@@ -26,7 +48,7 @@ if (isset($_POST['saveAdmin'])) {
     $phone = validate($_POST['phone']);
     $is_ban = validate($_POST['is_ban']) == true ? 1 : 0;
 
-    if ($name != '' && $email != "" && $password != "") {
+    if ($name !== '' && $email !== "" && $password !== "") {
         $emailCheck = mysqli_query($conn, "SELECT *FROM admins WHERE email='$email'");
         if ($emailCheck) {
             if (mysqli_num_rows($emailCheck) > 0) {
